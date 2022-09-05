74713abd9a1d9f02cd2e385ce7702bf219785c12
swu1337 <master_msp1@hotmail.com>
2022-09-05 10:52:40 +0200

    footer done

---
+++ components/Footer.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { ref } from 'vue'
+  const routes = ref([])
+
+  routes.value = [
+    {
+      name: 'Materialen',
+      url: '/materialen',
+    },
+    {
+      name: 'Care',
+      url: '/care',
+    },
+    {
+      name: 'Makers',
+      url: '/makers',
+    },
+    {
+      name: 'Ons verhaal',
+      url: '/ons-verhaal',
+    },
+    {
+      name: 'Over ons',
+      url: '/over-ons',
+    },
+  ]
+</script>
+
+<template>
+  <footer class=" bg-black py-11 lg:py-36">
+    <div class="container">
+      <ul class="flex flex-col gap-5 lg:flex-row lg:justify-around">
+        <li
+          v-for="(route, index) in routes"
+          :key="index"
+          class="text-white text-[14px] lg:text-[35px]"
+        >
+          <NuxtLink :to="route.url">{{ route.name }}</NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
