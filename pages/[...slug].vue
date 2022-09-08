bda41ed66060cddd829859adca085240e94898cb
Steven Wu <master_msp1@hotmail.com>
2022-09-08 11:40:59 +0200

    removed odd spacing

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -4,6 +4,6 @@
   const story = await useStoryblok(slug ? slug : 'home', { version: 'draft' })
 </script>
  
-<template> 
-    <StoryblokComponent v-if="story" :blok="story.content" />
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
 </template>
