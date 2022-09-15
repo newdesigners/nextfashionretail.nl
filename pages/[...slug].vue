c81bb5575663189b281da403b9b2051b9c8a6f40
swu1337 <master_msp1@hotmail.com>
2022-09-15 17:07:54 +0200

    favico added

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,6 +2,17 @@
   let { slug } = useRoute().params
   if (slug.length > 1) slug = slug.join('/')
   const story = await useStoryblok(slug ? slug : 'home', { version: 'draft' })
+
+  useHead({
+  title: 'Next Fashion Retail',
+  meta: [{
+    name: 'description',
+    content: 'Next Fashion Retail helpt jou met duurzamer worden'
+  }],
+  link: [{
+    
+  }]
+})
 </script>
  
 <template>
