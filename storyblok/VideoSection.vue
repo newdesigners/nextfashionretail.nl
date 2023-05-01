b38df5f233d9e2583e6be535204d2ef88b7bfded
Konrad Zwolinski <zwolinskikonrad@gmail.com>
2023-05-01 20:30:11 +0200

    Added first iteration of video on homepage (subject to change)

---
+++ storyblok/VideoSection.vue
@@ -0,0 +1,14 @@
+<script setup>
+  defineProps({ blok: Object })
+</script>
+
+<template>
+  <div 
+    v-editable="blok"
+    class="container pb-16 sm:pb-24 lg:pb-32 xl:pb-44"
+  >
+  <div class="h-[215px] md:h-[737px]">
+        <video class="w-full h-full object-cover" ref="video" v-if="blok?.src" :src="blok.src.filename" preload="metadata"></video>
+    </div>
+</div>
+</template>
