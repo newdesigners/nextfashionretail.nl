70374d01d8a059dc743a7a5c251c955572e4f804
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-09-01 09:54:54 +0200

    CardDownload/storyblok test 2#

--- storyblok/CardDownloadDemo.vue
+++ storyblok/CardDownloadDemo.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import TextWithButtonClone from '../components/TextWithButtonClone.vue';
 
 const props = defineProps({ blok: Object })
@@ -19,7 +19,7 @@ const components = {TextWithButtonClone};
           v-html="cardContent"
         >
       </article>
-      <!-- <div v-for="blok in props.blok.amount"> -->
+      
       <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-12">
         <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
       </aside>
@@ -27,7 +27,30 @@ const components = {TextWithButtonClone};
       <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
         <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
       </aside>
-      <!-- </div> -->
+
+      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-12">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
+      </aside>
+
+      <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
+      </aside>
+
+      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-12">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
+      </aside>
+
+      <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
+      </aside>
+
+      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-12">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
+      </aside>
+
+      <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
+      </aside>
 
       <component
           v-for="blok in props.blok.ButtonsWithText"
@@ -38,4 +61,43 @@ const components = {TextWithButtonClone};
 
     </div>
   </article>
+</template> -->
+
+<script setup>
+
+const props = defineProps({ blok: Object })
+const cardContent = computed(() => renderRichText(props.blok.description))
+</script>
+
+<template>
+  <article
+    v-editable="blok"
+    class="bg-white"
+  >
+    <div class="py-[26px] px-[30px] lg:px-[40px] lg:py-[32px] xl:p-12">
+      <h3 class="text-12 md:text-[35px] pb-2 lg:pb-3">{{ blok.title }}</h3>
+      <article
+          class="rich-text prose-sm md:prose-lg lg:prose-xl md:max-w-none"
+          v-html="cardContent"
+        >
+      </article>
+      
+      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-12">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
+      </aside>
+
+      <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
+        <DownloadButton :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
+      </aside>
+
+      <component
+          v-for="blok in props.blok.ButtonsWithText"
+          :key="blok._uid"
+          :blok="blok"
+          :is="blok.component"
+        />
+
+    </div>
+  </article>
 </template>
+
