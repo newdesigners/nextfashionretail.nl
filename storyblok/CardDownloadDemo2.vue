8443ac5e1771a818ba4bcc33a76bc59353bdb376
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-09-12 09:42:40 +0200

    Tailwind, code format changes to download button c

--- storyblok/CardDownloadDemo2.vue
+++ storyblok/CardDownloadDemo2.vue
@@ -18,15 +18,21 @@ const cardContent = computed(() => renderRichText(props.blok.description))
         >
       </article>
       
-      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-6">
+      <aside v-if="blok.ButtonIsLeft" class="flex flex-col items-start mt-4 md:mt-6 gap-6" >
         <DownloadButtonClone :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig }" />
+        <DownloadButtonClone :button="{ label : blok.button_label2, link : blok.file2, buttonIsBig : blok.buttonIsBig }" />
+        <DownloadButtonClone :button="{ label : blok.button_label3, link : blok.file3, buttonIsBig : blok.buttonIsBig }" />
+        <DownloadButtonClone :button="{ label : blok.button_label4, link : blok.file4, buttonIsBig : blok.buttonIsBig }" />
       </aside>
 
-      <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
+      <aside v-else class="flex flex-col justify-end mt-4 md:mt-12 items-end">
         <DownloadButtonClone :button="{ label : blok.button_label, link : blok.file, buttonIsBig : blok.buttonIsBig}"/>
+        <DownloadButtonClone :button="{ label : blok.button_label2, link : blok.file2, buttonIsBig : blok.buttonIsBig }" />
+        <DownloadButtonClone :button="{ label : blok.button_label3, link : blok.file3, buttonIsBig : blok.buttonIsBig }" />
+        <DownloadButtonClone :button="{ label : blok.button_label4, link : blok.file4, buttonIsBig : blok.buttonIsBig }" />
       </aside>
 
-      <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-6">
+      <!-- <aside v-if="blok.ButtonIsLeft" class="flex justify-start items-end mt-4 md:mt-6">
         <DownloadButtonClone :button="{ label : blok.button_label2, link : blok.file2, buttonIsBig : blok.buttonIsBig }" />
       </aside>
 
@@ -48,7 +54,7 @@ const cardContent = computed(() => renderRichText(props.blok.description))
 
       <aside v-else class="flex justify-end mt-4 md:mt-12 items-end">
         <DownloadButtonClone :button="{ label : blok.button_label4, link : blok.file4, buttonIsBig : blok.buttonIsBig}"/>
-      </aside>
+      </aside> -->
 <!-- 
       <component
           v-for="blok in props.blok.ButtonsWithText"
