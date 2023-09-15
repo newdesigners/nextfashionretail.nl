b6372febe991e288a374e0fa0ec581505ab50c06
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-09-15 15:37:22 +0200

    padding change

--- storyblok/CardLink.vue
+++ storyblok/CardLink.vue
@@ -7,7 +7,7 @@
     v-editable="blok"
     class="bg-white"
   >
-    <div class="py-[20px] px-[24px] lg:px-[40px] lg:py-[32px] xl:p-7">
+    <div class="py-[20px] px-[24px] lg:px-[38px] lg:py-[32px] xl:p-7">
       <h3 class="pb-2 lg:pb-3">{{ blok.title }}</h3>
       <p class="text-[12px] pb-4 sm:pb-5 xl:pb-6 lg:text-[14px] xl:text-[16px]">{{ blok.description }}</p>
       <aside class="flex justify-end">
