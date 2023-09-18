b2996c0a540502c1612e45a86ce0ac6bb808981e
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2023-09-18 14:16:22 +0200

    test

---
+++ storyblok/TitleText2.vue
@@ -0,0 +1,33 @@
+<script setup>
+  const props = defineProps({ blok: Object, isFirst: Boolean })
+  const articleContent = computed(() => renderRichText(props.blok.description)) 
+  // let articleContent;
+
+  // try {
+  //   articleContent = computed(() => renderRichText(props.blok.description))
+  // } catch (error) {
+  //   // Handle the error, e.g., log it or show a fallback content
+  //   console.error('Error rendering rich text:', error)
+  // }
+</script>
+
+<template>
+  <section
+    v-editable="blok"
+    :class="{ 'pt-6 lg:pt-16': isFirst }"
+  >
+    <div class="container pb-16 sm:text-center sm:w-[70%] sm:mx-auto xl:w-3/5">
+      <div class="pb-2">
+        <div v-if=isFirst>
+          <h1>{{ blok.title }}</h1>
+        </div>
+        <div v-else>
+          <h2>{{ blok.title }}</h2>
+        </div>
+      </div>
+      <article v-if="articleContent" class="rich-text prose-sm md:prose-lg lg:prose-xl md:max-w-none" v-html="articleContent">
+      </article>
+      <!-- <p>{{ blok.description }}</p> -->
+    </div>
+  </section>
+</template>
